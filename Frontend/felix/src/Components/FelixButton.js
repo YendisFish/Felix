02bb163961a100df2c92922b75a2194d26f95859
YendisFish <YendisFish@users.noreply.github.com>
2024-09-03 13:28:55 -0600

    prettyified code

---
+++ Frontend/felix/src/Components/FelixButton.js
@@ -0,0 +1,11 @@
+﻿import './felixbutton.css'
+
+function FelixButton({children, type}) {
+    return (
+        <div>
+            <button className="felix-button" type={type}>{children}</button>
+        </div>
+    )
+}
+
+export default FelixButton;
