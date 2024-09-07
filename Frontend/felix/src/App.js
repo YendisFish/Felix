1296971a5ea384dadb185f101227f11cfd0a7c47
YendisFish <YendisFish@users.noreply.github.com>
2024-09-07 08:36:18 +0000

    fixed everything bruh

--- Frontend/felix/src/App.js
+++ Frontend/felix/src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 import Login from './Components/Login.js';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage.js'
--- Frontend/felix/src/Components/Login.js
+++ Frontend/felix/src/Components/Login.js
@@ -1,39 +1,39 @@
 import { useState } from 'react';
-import './login.css';
+import '../CSS/login.css';
 import FelixButton from './FelixButton'
 
 function Login() {
     const [inputPassword, setInputPassword] = useState('');
     const [inputUsername, setInputUsername] = useState('');
-    
+
     const handleInputPassword = (event) => {
         setInputPassword(event.target.value);
     };
-    
+
     const handleInputUsername = (event) => {
         setInputUsername(event.target.value);
     };
-    
+
     const handleSubmit = (event) => {
         event.preventDefault();
         console.log('Username:', inputUsername);
         console.log('Password:', inputPassword);
     };
-    
+
     return (
         <div className="login-page">
-            <header> 
+            <header>
                 <h1 className="login-felix">Felix</h1>
             </header>
-        
+
             <div className="login-form">
                 <form onSubmit={handleSubmit}>
                     <h2 className='login-title'>Login</h2>
-                    <input value={inputUsername} onChange={handleInputUsername} placeholder="Username"/>
-                    
+                    <input className="login-input" value={inputUsername} onChange={handleInputUsername} placeholder="Username"/>
 
-                    <input type="password" value={inputPassword} onChange={handleInputPassword} placeholder="Password" />
-                    
+
+                    <input className="login-input" type="password" value={inputPassword} onChange={handleInputPassword} placeholder="Password" />
+
                     <FelixButton type="submit">Sign In</FelixButton>
                 </form>
             </div>
